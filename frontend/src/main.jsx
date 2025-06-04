f8d4d172b218ce5884bf26da8a26009ace1220c5
Nataliia Liubynets <natalialiubynets@gmail.com>
2025-06-04 10:00:59 +0200

    fix(vscode): Tailwind IntelliSense configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+
 import App from './App.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import './index.css';
 
 const router = createBrowserRouter([{ path: '/', Component: App }]);
 
